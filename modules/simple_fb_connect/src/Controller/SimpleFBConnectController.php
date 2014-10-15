843a401b403b710839e77c830c777967ebdb021e
Tanay Sai <saitanay@gmail.com>
2014-10-15 06:14:34 +0530

    Initial Commit . SFC module

---
+++ modules/simple_fb_connect/src/Controller/SimpleFBConnectController.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\page_example\Controller\PageExampleController.
+ */
+
+namespace Drupal\simple_fb_connect\Controller;
+
+use Drupal\Core\Url;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
